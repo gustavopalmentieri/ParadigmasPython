12cfad18d4eeb3ab9cc9bf011715dd31e3520bfa
Gustavo Jesus Palmentieri <gustavoo.palmentieri@gmail.com>
2025-10-14 22:45:10 -0300

    Delete atividade_01.py

--- atividade_01.py
+++
@@ -1,9 +0,0 @@
-camisetas = 39.90
-calças = 99.90
-
-qtd_camisetas = (float(input("Quantidade de camisetas que deseja:")))
-qtd_calças = (float(input("Quantidade de calças que deseja:")))
-
-total = (camisetas * qtd_camisetas) + (calças * qtd_calças)
-
-print ("Valor a ser pago: ", total)
